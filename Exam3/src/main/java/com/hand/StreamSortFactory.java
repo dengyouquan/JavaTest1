8028a0444a203d85730c7e8a18e4c77e410c6bbd
dengyouquan <youquan.deng@hand-china.com>
2018-08-23 13:06:59 +0800

    [FIX]完整版

--- Exam3/src/main/java/com/hand/StreamSortFactory.java
+++ Exam3/src/main/java/com/hand/StreamSortFactory.java
@@ -5,6 +5,15 @@ import java.util.function.Function;
 import java.util.stream.Collectors;
 import java.util.stream.*;
 
+class MyEntry{
+    private int key;
+    private int value;
+    public MyEntry(int key,int value) {
+        this.key = key;
+        this.value = value;
+    }
+}
+
 public class StreamSortFactory implements SortFactory {
     private static StreamSortFactory ourInstance = new StreamSortFactory();
 
@@ -15,33 +24,10 @@ public class StreamSortFactory implements SortFactory {
     private StreamSortFactory() {
     }
 
-    private static List<Integer> RandomList(int num, int bound) {
-        Random random = new Random();
-        List<Integer> list = new ArrayList<>();
-        while (num-- > 0) {
-            list.add(random.nextInt(bound));
-        }
-        //System.out.println(list.size());
-        //System.out.println(list);
-        return list;
-    }
-
-    Integer key(Integer i){
-        return i;
-    }
-
-    Integer value(Integer i){
-        return i/10;
-    }
-
     @Override
     public Map<Integer, List<Integer>> getList(int size, int bound) {
         Random random = new Random();
-        //get List
-        IntStream stream = random.ints(size, 0, bound);
-        List<Integer> keyList = Arrays.asList(1, 2, 3, 4, 5, 6, 7, 8, 9);
-        List<Integer> valueList = RandomList(size, bound);
-        //System.out.println(valueList.stream().collect(Collectors.toMap(key->key(key),value->value(value))));
-        return null;
+        Map<Integer, List<Integer>> map = random.ints(size, 0, bound).boxed().sorted().collect(Collectors.groupingBy(value->value/10));
+        return map;
     }
 }
